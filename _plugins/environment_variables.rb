44fad6ec52f5acf8a562b0f68908852d14e2cda6
Phil Martin <phil@gameplan.global>
2018-05-11 10:02:26 +1000

    added debugging for build

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -4,6 +4,7 @@ module Jekyll
     class EnvironmentVariablesGenerator < Generator
   
       def generate(site)
+        print "ENV['GOOGLE_ANALYTICS_UA'] = ",(ENV['GOOGLE_ANALYTICS_UA'] || "nothing")
         site.config['google_analytics_ua'] = ENV['GOOGLE_ANALYTICS_UA'] || 'UA-64177721-2'
         # Add other environment variables to `site.config` here...
       end
